45d4b8dbc5d59efdf03b081f6905c4d41d1477a6
Tharul <170612345+TharulJayasundara@users.noreply.github.com>
2024-05-28 20:14:20 +0530

    Add files via upload

---
+++ Clock/clock.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
+
+function setTime() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsRotation = seconds * 6; // 360/60
+  const minutesRotation = minutes * 6 + seconds * 0.1;            // 360/60 + fraction 
+  const hoursRotation = (hours % 12) * 30 + minutes * 0.5;        // 360/12 + 0.5
+
+  secondHand.style.transform = `rotate(${secondsRotation}deg)`;
+  minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+  hourHand.style.transform = `rotate(${hoursRotation}deg)`;
+}
+
+setInterval(setTime, 1000);
+setTime(); 
